8f674ecd939af0cdf82acbd4f7cd24ba867f2b0a
shefali <shefali@shaily>
2018-04-12 00:08:45 +0530

    Successfully loaded static resources

--- onlineshopping/src/main/java/net/sg/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/sg/onlineshopping/controller/PageController.java
@@ -15,5 +15,27 @@ public class PageController {
 	  mv.addObject("greeting","welcome to spring mvc");
 	  return mv;
   }
-
+  
+  /*@RequestMapping(value={"/test"})
+  public ModelAndView test(@RequestParam(value="greeting",required=false)String greeting)
+  {
+	  if(greeting==null)
+		  greeting="Hello there";
+	  ModelAndView mv=new ModelAndView("page");
+	  
+	  mv.addObject("greeting",greeting);
+	  return mv;
+  }*/
+  
+ /* @RequestMapping(value={"/test/{greeting}"})
+  public ModelAndView test(@PathVariable("greeting")String greeting)
+  {
+	  if(greeting==null)
+		  greeting="Hello there";
+	  ModelAndView mv=new ModelAndView("page");
+	  
+	  mv.addObject("greeting",greeting);
+	  return mv;
+  }
+*/
 }
