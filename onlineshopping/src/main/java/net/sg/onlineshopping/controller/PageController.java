547d1ee0e6c6707b9ab17f452cd55357573ce512
shefali <shefali@shaily>
2018-05-09 23:55:17 +0530

    SLF$J logging added

--- onlineshopping/src/main/java/net/sg/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/sg/onlineshopping/controller/PageController.java
@@ -1,5 +1,7 @@
 package net.sg.onlineshopping.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +16,7 @@ import net.sg.shoppingbackend.dto.Product;
 @Controller
 public class PageController {
 	
-	
+	private static final Logger logger=LoggerFactory.getLogger(PageController.class);
 	@Autowired
 	private CategoryDAO categoryDAO;
 	
@@ -24,6 +26,8 @@ public class PageController {
   public ModelAndView index()
   {
 	  ModelAndView mv=new ModelAndView("page");
+	  logger.info("inside page controller index page-INFO");
+	  logger.debug("Inside page controller index page-Debug");
 	  
 	  mv.addObject("title","Home");
 	  
