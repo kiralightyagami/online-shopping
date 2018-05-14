723880dd56ced6c7959b855c203d84964c3b396b
shefali <shefali@shaily>
2018-05-14 20:33:15 +0530

    COmpelted file upload

--- onlineshopping/src/main/java/net/sg/onlineshopping/util/FileUploadUtility.java
+++ onlineshopping/src/main/java/net/sg/onlineshopping/util/FileUploadUtility.java
@@ -1,12 +1,13 @@
 package net.sg.onlineshopping.util;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
-
-import net.sg.onlineshopping.controller.ManagementController;
 
 public class FileUploadUtility {
     //replace all forward slash in path to either double slash or backward slash for escape sequences
@@ -18,6 +19,28 @@ public class FileUploadUtility {
 		REAL_PATH=request.getSession().getServletContext().getRealPath("/assests/images/");
 		logger.info(REAL_PATH);
 		//to make sure all directories exist if not then create directories
+		if(!new File(ABS_PATH).exists())
+		{
+			//create the dir
+			new File(ABS_PATH).mkdirs();
+		}
+		if(!new File(REAL_PATH).exists())
+		{
+			//create the dir
+			new File(REAL_PATH).mkdirs();
+		}
+		
+		try {
+			
+			//server upload
+			file.transferTo(new File(REAL_PATH+code+".jpg"));
+			//project dir upload
+			file.transferTo(new File(ABS_PATH+code+".jpg"));
+		}catch(IOException ex)
+		{
+			
+			
+		}
 		
 		
 	}
